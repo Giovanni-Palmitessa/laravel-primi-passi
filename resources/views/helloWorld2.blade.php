df7d9716a7bd1331b169195ecac2280cf7630a8a
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-22 15:50:47 +0200

    add route for helloworld 2

---
+++ resources/views/helloWorld2.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+
+<body>
+    <h1 style="text-align: center;">Questo è il secondo Hello World di Laravel!</h1>
+    <img src="https://turismo.puglia.it/wp-content/uploads/sites/124/monopoli-hd.jpg" alt="Il posto più bello del mondo">
+</body>
+
+</html>
