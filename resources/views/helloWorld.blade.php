2e1a647a25c7f912165a3be3e456d8ba27035609
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-22 15:47:03 +0200

    add h1 and img for test

--- resources/views/helloWorld.blade.php
+++ resources/views/helloWorld.blade.php
@@ -8,7 +8,8 @@
 </head>
 
 <body>
-    <h1>Questo è il primo Hello World di Laravel!</h1>
+    <h1 style="text-align: center;">Questo è il primo Hello World di Laravel!</h1>
+    <img src="https://yt3.googleusercontent.com/UXFC9eFKxjbOcUeEiIFqpywdInXJijIrh5bkfUMPlKhCLKaO6rrTfh5O5IzNTS_2Wap2yBk1J44=s900-c-k-c0x00ffffff-no-rj" alt="Boolean" style="height: 400px;">
 </body>
 
 </html>
